8c1d015903963c9a2673a641cb74f6ea7791f488
Zivisio <40009483+Zivisio@users.noreply.github.com>
2022-11-21 17:59:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,35 +7,122 @@ class Ui(QtWidgets.QMainWindow):
         super(Ui, self).__init__()
         self.mainMenu()
     def mainMenu(self):
-        uic.loadUi('test.ui', self)
+        uic.loadUi('maindev.ui', self)
         self.show()
+        #self.resetcolors()
         #when button is clicked go to different ui
         self.pushButton.clicked.connect(self.goToYes)
         self.pushButton_2.clicked.connect(self.goToNo)
         self.pushButton_3.clicked.connect(self.goToMaybe)
-        self.pushButton_4.clicked.connect(self.goToOther)
+        self.pushButton_4.clicked.connect(self.optionsPage)
     def goToYes(self):
-        self.pushButton.setStyleSheet("background-color: rgb(255, 255, 255);")
-        QTimer.singleShot(1000, self.resetcolors)
-    def goToNo(self):
-        self.pushButton_2.setStyleSheet("background-color: rgb(255, 255, 255);")
-        QTimer.singleShot(1000, self.resetcolors)
-    def goToMaybe(self):
-        self.pushButton_3.setStyleSheet("background-color: rgb(255, 255, 255);")
-        QTimer.singleShot(1000, self.resetcolors)
-    def goToOther(self):
-        self.pushButton_4.setStyleSheet("background-color: rgb(255, 255, 255);")
-        QTimer.singleShot(1000, self.resetcolors)
-    def resetcolors(self):
-        self.pushButton.setStyleSheet("background-color: rgb(106, 106, 255);")
-        self.pushButton_2.setStyleSheet("background-color: rgb(255, 55, 55);")
-        self.pushButton_3.setStyleSheet("background-color: rgb(255, 255, 28);")
-        self.pushButton_4.setStyleSheet("background-color: rgb(130, 130, 130);")
-        self.mainMenu
+        if(flashEnabled):
+            self.flash(1)
+        if(doubleClickEnabled):
+            self.pushButton.clicked.connect(self.goToYes2)
+        else:
+            self.gotoYes2()
+    
+    def gotoYes2(self):
+        #say yes
+        self.resetcolors()
 
+    def goToNo(self):
+        if(flashEnabled):
+            self.flash(2)
+        if(doubleClickEnabled):
+            self.pushButton_2.clicked.connect(self.goToNo2)
+        else:
+            self.gotoNo2()
+
+    def gotoNo2(self):
+        #say no
+        self.resetcolors()
+
+    def goToMaybe(self):
+        if(flashEnabled):
+            self.flash(3)
+        if(doubleClickEnabled):
+            self.pushButton_3.clicked.connect(self.goToMaybe2)
+        else:
+            self.gotoMaybe2()
+
+    def gotoMaybe2(self):
+        #say maybe
+        self.resetcolors()
+
+    def optionsPage(self):
+        self.hide()
+        self.options = options()
+        self.options.show()
+        
+    def resetcolors(self):
+        if(colorBlindEnabled):
+            self.pushButton.setStyleSheet(colorBlindGreen)
+            self.pushButton_2.setStyleSheet(colorBlindRed)
+            self.pushButton_3.setStyleSheet(colorBlindYellow)
+            self.pushButton_4.setStyleSheet(colorBlindGrey)
+        else:
+            self.pushButton.setStyleSheet(Green)
+            self.pushButton_2.setStyleSheet(Red)
+            self.pushButton_3.setStyleSheet(Yellow)
+            self.pushButton_4.setStyleSheet(Grey)
+        self.mainMenu()
+    
+    def flash(self, buttonNum):
+        if(buttonNum == 1):
+            self.pushButton.setStyleSheet(flashColor)
+        if(buttonNum == 2):
+            self.pushButton_2.setStyleSheet(flashColor)
+        if(buttonNum == 3):
+            self.pushButton_3.setStyleSheet(flashColor)
+        if(buttonNum == 4):
+            self.pushButton_4.setStyleSheet(flashColor)
+        QTimer.singleShot(flashTime, self.resetcolors)
+        
+class options(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(options, self).__init__()
+        uic.loadUi('options.ui', self)
+        self.show()
+        self.flashBox.setChecked(flashEnabled)
+        self.doubleBox.setChecked(doubleClickEnabled)
+        self.cbBox.setChecked(colorBlindEnabled)
+        self.optionsPage()
+    def optionsPage(self):
+        self.pushButton.clicked.connect(self.mainMenu)
+        self.flashBox.stateChanged.connect(self.somethingChanged)
+        self.doubleBox.stateChanged.connect(self.somethingChanged)
+        self.cbBox.stateChanged.connect(self.somethingChanged)
+    def somethingChanged(self):
+        global flashEnabled
+        global doubleClickEnabled
+        global colorBlindEnabled
+        flashEnabled = self.flashBox.isChecked()
+        doubleClickEnabled = self.doubleBox.isChecked()
+        colorBlindEnabled = self.cbBox.isChecked()
+        self.optionsPage()
+    def mainMenu(self):
+        self.hide()
+        self.main = Ui()
+        self.main.show()
+
+doubleClickEnabled = False
+flashEnabled = False
+firstLoad = True
+flashTime = 500
+ttsEnabled = True
+colorBlindEnabled = True
+flashColor = "background-color: rgb(255, 255, 255);"
+colorBlindRed="background-color: rgb(255, 55, 55);"
+colorBlindGreen ="background-color: rgb(106, 106, 255);"
+colorBlindYellow="background-color: rgb(255, 255, 28);"
+colorBlindGrey="background-color: rgb(130, 130, 130);"
+Red="background-color: rgb(255, 0, 0);"
+Green="background-color: rgb(0, 255, 0);"
+Yellow="background-color: rgb(255, 255, 0);"
+Grey="background-color: rgb(128, 128, 128);"
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
-window.setFixedHeight(800)
-window.setFixedWidth(450)
 app.exec_()
